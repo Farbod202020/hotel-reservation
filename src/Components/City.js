26ddd62a097c15bbe81c089531f8c48f0cf01dfb
Farbod salmani <143803764+Farbod202020@users.noreply.github.com>
2025-01-06 02:49:09 +0300

    City.js

--- src/Components/City.js
+++ src/Components/City.js
@@ -1,36 +1,33 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { Carousel } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
 const City = ({ data }) => {
-    return (
-        <div className="container c-carousel-location">
-        <div className="row justify-content-center">
-            <div className="col-md-8">
-            <Carousel prevIcon={<FontAwesomeIcon icon={faAngleLeft} />} nextIcon={<FontAwesomeIcon icon={faAngleRight} />} 
-
-                        className="horizontal-carousel"
-            >
+  return (
+    <div className="container c-carousel-location">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <Carousel prevIcon={<FontAwesomeIcon icon={faAngleLeft} />} nextIcon={<FontAwesomeIcon icon={faAngleRight} />} className="horizontal-carousel">
             {data.map((slide, index) => (
-                <Carousel.Item key={index}>
-                    <div className="row">
-                        {slide.map((item, itemIndex) => (
-                            <div className="col-md-3 box-detail" key={itemIndex}>
-                                <img src={item.image} className="img-fluid img-size" alt={`Slide ${index + 1}`} />
-                                <div className="text-center">{item.text}</div>
-                                <div className="text-center">{item.city}</div>
-                            </div>
-                        ))}
+              <Carousel.Item key={index}>
+                <div className="row">
+                  {slide.map((item, itemIndex) => (
+                    <div className="col-md-3 box-detail" key={itemIndex}>
+                      <img src={item.image} className="img-fluid img-size" alt={`Slide ${index + 1}`} />
+                      <div className="text-center">{item.text}</div>
+                      <div className="text-center">{item.city}</div>
                     </div>
-                </Carousel.Item>
+                  ))}
+                </div>
+              </Carousel.Item>
             ))}
-        </Carousel>
+          </Carousel>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
-  
-  export default City;
 
+export default City;
