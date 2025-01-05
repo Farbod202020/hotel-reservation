26c2be332b8d43113023fe97fbd34e5bcd642b5f
Farbod salmani <143803764+Farbod202020@users.noreply.github.com>
2025-01-06 02:43:14 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+
+// Lazy load the HomePage component
+const HomePage = lazy(() => import('./pages/HomePage'));
+
 function App() {
-    return (
-      <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-      </Router>
-    );
-  }
-  
-  export default App;
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default App;
