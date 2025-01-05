6018088a515b06d6657ff2a0af4121bb6bc4e7b5
Farbod salmani <143803764+Farbod202020@users.noreply.github.com>
2025-01-06 02:44:15 +0300

    Script.js

--- public/Script.js
+++ public/Script.js
@@ -1,5 +1,9 @@
 import $ from 'jquery';
-$('.datepicker').datepicker({
+import 'bootstrap-datepicker';
+
+$(document).ready(function() {
+  $('.datepicker').datepicker({
     format: 'mm/dd/yyyy',
     startDate: '-3d'
+  });
 });
